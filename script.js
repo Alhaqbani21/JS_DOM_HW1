93d427e909f96d64e93057b48eaf6fb2d3128b29
Abdulaziz Alhaqbani <alhaqbani21@gmail.com>
2024-05-27 14:22:12 +0300

    update classList toggle

--- script.js
+++ script.js
@@ -2,125 +2,128 @@ function showImage() {
   const img1 = document.getElementById('profileImage');
   const img2 = document.getElementById('profileImage2');
 
-  img1.classList.add('hidden');
-  img2.classList.remove('hidden');
+  // img1.classList.add('hidden');
+  // img2.classList.remove('hidden');
+
+  img1.classList.toggle('hidden');
+  img2.classList.toggle('hidden');
 }
 
 function hideImage() {
   const img1 = document.getElementById('profileImage');
   const img2 = document.getElementById('profileImage2');
 
-  img1.classList.remove('hidden');
-  img2.classList.add('hidden');
+  img1.classList.toggle('hidden');
+  img2.classList.toggle('hidden');
 }
 
 function showContact() {
   let contact1 = document.getElementById('contact1');
   let contact2 = document.getElementById('contact2');
-  contact1.classList.add('hidden');
-  contact2.classList.remove('hidden');
+  contact1.classList.toggle('hidden');
+  contact2.classList.toggle('hidden');
 }
 function hideContact() {
   let contact1 = document.getElementById('contact1');
   let contact2 = document.getElementById('contact2');
-  contact1.classList.remove('hidden');
-  contact2.classList.add('hidden');
+  contact1.classList.toggle('hidden');
+  contact2.classList.toggle('hidden');
 }
 
 function showEducation() {
   let education1 = document.getElementById('education1');
   let education2 = document.getElementById('education2');
-  education1.classList.add('hidden');
-  education2.classList.remove('hidden');
+  education1.classList.toggle('hidden');
+  education2.classList.toggle('hidden');
 }
 function hideEducation() {
   let education1 = document.getElementById('education1');
   let education2 = document.getElementById('education2');
-  education1.classList.remove('hidden');
-  education2.classList.add('hidden');
+  education1.classList.toggle('hidden');
+  education2.classList.toggle('hidden');
 }
 
 function showSkills() {
   let listSkills1 = document.getElementById('listSkills1');
   let listSkills2 = document.getElementById('listSkills2');
 
-  listSkills1.classList.add('hidden');
-  listSkills2.classList.remove('hidden');
+  listSkills1.classList.toggle('hidden');
+  listSkills2.classList.toggle('hidden');
 }
 
 function hideSkills() {
   let listSkills1 = document.getElementById('listSkills1');
   let listSkills2 = document.getElementById('listSkills2');
-  listSkills1.classList.remove('hidden');
-  listSkills2.classList.add('hidden');
+  listSkills1.classList.toggle('hidden');
+  listSkills2.classList.toggle('hidden');
 }
 function showLanguages() {
   let languageList1 = document.getElementById('languageList1');
   let languageList2 = document.getElementById('languageList2');
 
-  languageList1.classList.add('hidden');
-  languageList2.classList.remove('hidden');
+  languageList1.classList.toggle('hidden');
+  languageList2.classList.toggle('hidden');
 }
 
 function hideLanguages() {
   let languageList1 = document.getElementById('languageList1');
   let languageList2 = document.getElementById('languageList2');
-  languageList1.classList.remove('hidden');
-  languageList2.classList.add('hidden');
+  languageList1.classList.toggle('hidden');
+  languageList2.classList.toggle('hidden');
 }
 function showName() {
   let nameSection1 = document.getElementById('nameSection1');
   let nameSection2 = document.getElementById('nameSection2');
 
-  nameSection1.classList.add('hidden');
-  nameSection2.classList.remove('hidden');
+  nameSection1.classList.toggle('hidden');
+  nameSection2.classList.toggle('hidden');
 }
 
 function hideName() {
   let nameSection1 = document.getElementById('nameSection1');
   let nameSection2 = document.getElementById('nameSection2');
-  nameSection1.classList.remove('hidden');
-  nameSection2.classList.add('hidden');
+  nameSection1.classList.toggle('hidden');
+  nameSection2.classList.toggle('hidden');
 }
 function showProfile() {
   let profileContent1 = document.getElementById('profileContent1');
   let profileContent2 = document.getElementById('profileContent2');
 
-  profileContent1.classList.add('hidden');
-  profileContent2.classList.remove('hidden');
+  profileContent1.classList.toggle('hidden');
+  profileContent2.classList.toggle('hidden');
 }
 
 function hideProfile() {
   let profileContent1 = document.getElementById('profileContent1');
   let profileContent2 = document.getElementById('profileContent2');
-  profileContent1.classList.remove('hidden');
-  profileContent2.classList.add('hidden');
+  profileContent1.classList.toggle('hidden');
+  profileContent2.classList.toggle('hidden');
 }
 function showExperience() {
   let experience1 = document.getElementById('experience1');
   let experience2 = document.getElementById('experience2');
 
-  experience1.classList.add('hidden');
-  experience2.classList.remove('hidden');
+  experience1.classList.toggle('hidden');
+  experience2.classList.toggle('hidden');
 }
 
 function hideExperience() {
   let experience1 = document.getElementById('experience1');
   let experience2 = document.getElementById('experience2');
-  experience1.classList.remove('hidden');
-  experience2.classList.add('hidden');
+  experience1.classList.toggle('hidden');
+  experience2.classList.toggle('hidden');
 }
 function showRefrence() {
   let reference1 = document.getElementById('reference1');
   let reference2 = document.getElementById('reference2');
 
-  reference1.classList.add('hidden');
-  reference2.classList.remove('hidden');
+  reference1.classList.toggle('hidden');
+  reference2.classList.toggle('hidden');
 }
 
 function hideRefrence() {
   let reference1 = document.getElementById('reference1');
   let reference2 = document.getElementById('reference2');
-  reference1.classList.remove('hidden');
-  reference2.classList.add('hidden');
+  reference1.classList.toggle('hidden');
+  reference2.classList.toggle('hidden');
 }
